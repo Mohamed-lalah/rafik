6329ab01ed6263f28c2c2f20b3a3d4d0b759daa3
Mohamed-lalah <mohamedlalah918@gmail.com>
2024-01-05 00:22:07 +0200

    Memory screen Photo (upload and take added )

--- lib/ui/screens/memory_screen.dart
+++ lib/ui/screens/memory_screen.dart
@@ -21,7 +21,7 @@ class MemoryScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             SizedBox(
-              height: height*0.15,
+              height: height*0.13,
             ),
             Center(
               child: Text("Memory Recovery Upload ",
@@ -29,7 +29,7 @@ class MemoryScreen extends StatelessWidget {
             ),
 
             SizedBox(
-              height: height*0.1,
+              height: height*0.05,
             ),
             Column(
              children: [
@@ -41,7 +41,7 @@ class MemoryScreen extends StatelessWidget {
                    style: AppTheme.memory_Sub_TitleStyle),
              ],
            ),
-            SizedBox(height: height*0.06,),
+            SizedBox(height: height*0.04,),
             Row(
               children: [
                 Padding(
@@ -72,6 +72,7 @@ class MemoryScreen extends StatelessWidget {
                         Memories(path:"assets/images/photo3 (1X).png" ,)
                       ],
                     ),
+                    SizedBox(height: height*0.013,),
                     Row(
                       children: [
                         Memories(path:"assets/images/photo1(1x).png" ,),
@@ -81,6 +82,7 @@ class MemoryScreen extends StatelessWidget {
                         Memories(path:"assets/images/photo3 (1X).png" ,)
                       ],
                     ),
+                    SizedBox(height: height*0.013,),
                     Row(
                       children: [
                         Memories(path:"assets/images/photo1(1x).png" ,),
@@ -94,6 +96,65 @@ class MemoryScreen extends StatelessWidget {
                   ],
                 ),
               ),
+            ),
+            SizedBox(height: height*0.02,),
+            Row(
+              children: [
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    height: 39,
+                    width: 365,
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                          MaterialStateProperty.all<Color>(AppColors.login_Button_Color),
+                        ),
+                        onPressed: (){},
+                        child:Row(
+                          children: [
+                            Text("Upload Photo",style: AppTheme.memory_Sub_TitleStyle
+                            .copyWith(
+                        fontWeight: FontWeight.w600,
+                            color: AppColors.white
+
+                        ),),
+                            Spacer(),
+                            Icon(Icons.photo_library_outlined)
+                          ],
+                        ) ),
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    height: 39,
+                    width: 365,
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                          MaterialStateProperty.all<Color>(AppColors.login_Button_Color),
+                        ),
+                        onPressed: (){},
+                        child:Row(
+                          children: [
+                            Text("Take Photo",style: AppTheme.memory_Sub_TitleStyle
+                              .copyWith(
+                              fontWeight: FontWeight.w600,
+                              color: AppColors.white,
+                              fontSize: 15
+
+                            ),),
+                            Spacer(),
+                            Icon(Icons.camera_alt_outlined)
+                          ],
+                        )
+                         ),
+                  ),
+                ),
+              ],
             )
           ],
         ),

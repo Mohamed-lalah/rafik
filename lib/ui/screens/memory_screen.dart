201749a4e536cb261da525db46c34b9b3c36c3c8
Mohamed-lalah <mohamedlalah918@gmail.com>
2024-01-04 23:51:55 +0200

    Memory Screen design finished

--- lib/ui/screens/memory_screen.dart
+++ lib/ui/screens/memory_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../utlities/app_colors.dart';
 import '../utlities/app_theme.dart';
+import '../widgets/memories_widget.dart';
 
 class MemoryScreen extends StatelessWidget {
   const MemoryScreen({super.key});
@@ -14,50 +15,88 @@ class MemoryScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.white,
       body:
-      Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          SizedBox(
-            height: height*0.15,
-          ),
-          Center(
-            child: Text("Memory Recovery Upload ",
-              style: AppTheme.memoryTitleStyle,),
-          ),
+      SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              height: height*0.15,
+            ),
+            Center(
+              child: Text("Memory Recovery Upload ",
+                style: AppTheme.memoryTitleStyle,),
+            ),
 
-          SizedBox(
-            height: height*0.1,
-          ),
-          Column(
-           children: [
-             Text("This section is responsible For Visual ",
-             style: AppTheme.memory_Sub_TitleStyle),
-             Text("Remebering To Help You To Recover ",
-                 style: AppTheme.memory_Sub_TitleStyle),
-             Text("The Lost Part Of Your Memory",
-                 style: AppTheme.memory_Sub_TitleStyle),
-           ],
-         ),
-          SizedBox(height: height*0.1,),
-          Row(
-            children: [
-              Padding(
-                  padding: EdgeInsets.only(left: width*0.05),
-                  child: Text("My Memories",style: AppTheme.memoryTitleStyle
-                    .copyWith(fontWeight: FontWeight.w600,
-                  fontSize: 16),)),
-              Spacer(),
-              IconButton(
-                icon: Icon(Icons.more_vert,color: AppColors.memory_button_color,),
-                onPressed: () {
-                  // Handle button press
-                },
-              )
+            SizedBox(
+              height: height*0.1,
+            ),
+            Column(
+             children: [
+               Text("This section is responsible For Visual ",
+               style: AppTheme.memory_Sub_TitleStyle),
+               Text("Remebering To Help You To Recover ",
+                   style: AppTheme.memory_Sub_TitleStyle),
+               Text("The Lost Part Of Your Memory",
+                   style: AppTheme.memory_Sub_TitleStyle),
+             ],
+           ),
+            SizedBox(height: height*0.06,),
+            Row(
+              children: [
+                Padding(
+                    padding: EdgeInsets.only(left: width*0.05),
+                    child: Text("My Memories",style: AppTheme.memoryTitleStyle
+                      .copyWith(fontWeight: FontWeight.w600,
+                    fontSize: 16),)),
+                Spacer(),
+                IconButton(
+                  icon: Icon(Icons.more_vert,color: AppColors.memory_button_color,),
+                  onPressed: () {
+                    // Handle button press
+                  },
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Memories(path:"assets/images/photo1(1x).png" ,),
+                        SizedBox(width: width*0.02,),
+                        Memories(path:"assets/images/photo2(1X).png" ,),
+                        SizedBox(width: width* 0.02,),
+                        Memories(path:"assets/images/photo3 (1X).png" ,)
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Memories(path:"assets/images/photo1(1x).png" ,),
+                        SizedBox(width: width*0.02,),
+                        Memories(path:"assets/images/photo2(1X).png" ,),
+                        SizedBox(width: width* 0.02,),
+                        Memories(path:"assets/images/photo3 (1X).png" ,)
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Memories(path:"assets/images/photo1(1x).png" ,),
+                        SizedBox(width: width*0.02,),
+                        Memories(path:"assets/images/photo2(1X).png" ,),
+                        SizedBox(width: width* 0.02,),
+                        Memories(path:"assets/images/photo3 (1X).png" ,)
+                      ],
+                    ),
 
-            ],
-          )
-        ],
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       )
       
      
